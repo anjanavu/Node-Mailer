93ba5f442f4dd1f6d13bcbf9383779c167669935
Anjana V U <anjanavu2000@gmail.com>
2023-10-31 17:18:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const nodemailer=require('nodemailer');
+let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'vuanjana2000@gmail.com',
+        pass:'vlhd lyhh uxvc raeu'
+    }
+});
+
+let mailDetails = {
+    from: process.env.EMAIL_USER,
+    to: 'anjanavu2000@gmail.com',
+    subject: 'Test mail',
+    text: 'Node.js testing mail'
+};
+
+mailTransporter.sendMail(mailDetails, function(err, data) {
+    if (err) {
+        console.log('Error Occurs', err);
+    } else {
+        console.log('Email sent successfully');
+    }
+});
+
+
+
+const PORT = 3541;
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
